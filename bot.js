cd9b2db74fc0970618726248e5522bbd2536c802
onocmouse <onocmouse@gmail.com>
2016-04-02 15:47:00 -0500

    Now using twitter-bot-environment, the npm module we wrote to do bot setup.

--- bot.js
+++ bot.js
@@ -1,16 +1,13 @@
 var lodash = require('lodash');
 var twitter = require('twode');
-//var RiTa = require('rita');
 var Copia = require('./lib/Copia');
+var BotEnvironment = require('twitter-bot-environment');
+
+var environment = new BotEnvironment();
 
 function Bot(handle) {
 	this.handle = handle.charAt(0) === '@' ? handle : '@' + handle;
-	this.twitter = new twitter({
-		consumer_key: process.env.CONSUMER_KEY,
-		consumer_secret: process.env.CONSUMER_SECRET,
-		access_token_key: process.env.ACCESS_TOKEN_KEY,
-		access_token_secret: process.env.ACCESS_TOKEN_SECRET
-	});
+	this.twitter = new twitter(environment.getEnvironment());
 	this.copia = new Copia();
 }
 
@@ -18,11 +15,6 @@ Bot.prototype.run = function() {
 	this.twitter.updateStatus(this.copia.get_copia(1), function (err, data) {
       console.log(err, data);
     })
-	//setInterval(function() {
-	//	this.twitter.updateStatus(this.copia.get_copia(1), function (err, data) {
-	//      console.log(err, data);
-	//    });
-	//}.bind(this), 60 * 60 * 1000);
 }
 
 var copia = new Bot('HomeworkCopia');
